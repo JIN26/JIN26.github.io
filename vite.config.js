0af34d3684103b9ddecbdcbfc998882efd262642
Angel Mantilla <32878957+JIN26@users.noreply.github.com>
2023-11-19 01:12:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ruranjo.github.io/portafoliorubens.github.io"
+  base: "https://JIN26.github.io/portafolio.github.io"
 })
